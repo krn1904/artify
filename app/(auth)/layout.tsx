15506d6c30e6eab9ad2aec0028161c783d7fd8d7
krn_1904 <Karanssoni2002@gmail.com>
2025-09-18 13:58:41 +1000

    Fix scroll issue on Login/Signup page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="box-border flex min-h-[calc(100svh-4rem-1px)] -mt-2 md:-mt-4 w-full items-center justify-center px-4 sm:px-6 lg:px-8 py-0">
       <div className="w-full max-w-md space-y-8">
         {children}
       </div>
