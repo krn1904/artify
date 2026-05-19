d34dd6af278e9c938833a85ef8dbf8d29002990b
krn_1904 <Karanssoni2002@gmail.com>
2026-05-19 22:26:02 +1000

    build fixed

--- lib/auth/authOptions.ts
+++ lib/auth/authOptions.ts
@@ -1,7 +1,7 @@
 import { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { compare } from 'bcryptjs';
-import { getMongoDatabase } from './db';
+import { getMongoDatabase } from '../db';
 
 export const authOptions: NextAuthOptions = {
   providers: [
